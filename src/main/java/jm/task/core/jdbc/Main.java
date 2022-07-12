d543a6352d49f0421457dd393d2ee86a128b16a1
Vladlistopad999 <vlad.topalov.1996@inbox.ru>
2022-07-12 21:25:54 +0500

    Вроде норм

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,11 +1,14 @@
 package jm.task.core.jdbc;
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        UserDao userDao = new UserDaoJDBCImpl();
+    public static void main(String[] args) {
+        UserService userDao = new UserServiceImpl();
         userDao.createUsersTable();
         userDao.saveUser("Виктор", "Калянов", (byte) 28);
         userDao.saveUser("Иван", "Сиднев", (byte) 28);
@@ -18,6 +21,12 @@ public class Main {
         System.out.println(userDao.getAllUsers());
         userDao.cleanUsersTable();
         System.out.println(userDao.getAllUsers());
-        userDao.dropUsersTable();
+        try {
+            userDao.dropUsersTable();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
